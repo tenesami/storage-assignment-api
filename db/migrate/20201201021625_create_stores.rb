ef39a033d1c5e1fd012ebee5fccefaf2b845baab
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-11-30 19:45:00 -0700

    create store and user data base

---
+++ db/migrate/20201201021625_create_stores.rb
@@ -0,0 +1,11 @@
+class CreateStores < ActiveRecord::Migration[6.0]
+  def change
+    create_table :stores do |t|
+      t.string :item
+      t.text :description
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
